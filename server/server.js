15007e9810a7bcca9291f7f80e79fa921010d7a1
Salim <salimaliahmad@outlook.com>
2018-07-01 17:14:23 +0300

    Create generateMessage utility

--- server/server.js
+++ server/server.js
@@ -4,9 +4,12 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
+
 // so instead of going into server, then going out of it, then going to public, we go straight to public folder
 const publicPath = path.join(__dirname, '../public'); // ...6-ChatApp/public
 const port = process.env.PORT || 3000;
+
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
@@ -19,26 +22,14 @@ io.on('connection', (socket) => {
     // socket.emit() emits an event to a single connection
     // io.emit() emits an event to every single connection
     // Broadcasting an event will send it to all the other sockets in the namespace except for yourself
-    socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to the chat app',
-        createdAt: new Date().getTime()
-    })
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: 'New user joined',
-        createdAt: new Date().getTime()
-    })
-    
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user has joined'));
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
 
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()
-        })
+        io.emit('newMessage', generateMessage(message.from, message.text));
     })
 
     socket.on('disconnect', () => {
