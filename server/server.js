56f8fd5d6f3998826ffd936f1de069c6d0b1b586
Salim <salimaliahmad@outlook.com>
2018-07-01 13:34:25 +0300

    Add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 // we're using path to make it easier to go from one directory to another instead of exiting one then going to another
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 // so instead of going into server, then going out of it, then going to public, we go straight to public folder
 const publicPath = path.join(__dirname, '../public'); // ...6-ChatApp/public
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User was disconnected');
+    })
+})
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 })
